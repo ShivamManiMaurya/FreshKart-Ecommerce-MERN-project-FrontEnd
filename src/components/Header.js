4069e3f0f4a3cd6af731d7fde5ad653ea39aa8b4
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2023-06-03 23:28:01 +0530

    refactor - icon and other changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "../assets/images/logo/megamart choice.png";
+import logo from "../assets/images/logo/logo.png";
 
 function Header() {
     return (
-        <header>
+        <header className=" fixed shadow-md w-full h-[70px] bg-black px-4 md:px-16">
             <div>
                 <div>
-                    <img src={logo} alt="logo" />
+                    <img src={logo} alt="logo" className="h-[70px]" />
                 </div>
             </div>
         </header>
