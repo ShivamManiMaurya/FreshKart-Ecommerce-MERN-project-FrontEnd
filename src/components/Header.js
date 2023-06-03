2ce33949e64448f7a9c5e0488e0401cc8e96b673
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2023-06-03 15:01:26 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "../assets/images/logo/megamart choice.png";
+
+function Header() {
+    return (
+        <header>
+            <div>
+                <div>
+                    <img src={logo} alt="logo" />
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
