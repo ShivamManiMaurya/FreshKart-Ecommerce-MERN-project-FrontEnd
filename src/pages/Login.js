11d6ba5e6572020e890de62c2ceb98f86fbd342c
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2023-06-05 23:12:26 +0530

    feat - signup and login logic done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import loginGifAnimation from "../assets/images/logo/loginLogo.gif";
+import { Link } from "react-router-dom";
 
 function Login() {
-    return <div>Login</div>;
+    const [data, setData] = useState({
+        email: "",
+        password: "",
+    });
+    const [checkRequiredField, setCheckReqField] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { email, password } = data;
+        if (email && password) {
+            setCheckReqField(false);
+            console.log(data);
+            alert("success");
+        } else {
+            setCheckReqField(true);
+            // alert("Check Required * fields first");
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setData((prev) => {
+            return {
+                ...prev,
+                [name]: value,
+            };
+        });
+    };
+
+    return (
+        <div className="flex h-full justify-center items-center">
+            <div className=" pt-3 w-full max-w-sm bg-white rounded-sm shadow-sm shadow-zinc-500">
+                <div className="w-[100px] pb-0 m-auto">
+                    <img
+                        src={loginGifAnimation}
+                        alt=""
+                        className=" animate-wiggle"
+                    />
+                </div>
+                <form action="" className=" mx-8 pb-4" onSubmit={handleSubmit}>
+                    <label htmlFor="email" className="pl-2 font-bold">
+                        Email<span className="text-red-700">*</span>
+                        <span className=" text-sm font-normal">
+                            (Indicates required field)
+                        </span>
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Email"
+                        className="w-full h-10 bg-zinc-200 rounded-sm px-2 shadow-sm shadow-zinc-500 mb-2"
+                        value={data.email}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="email" className="pl-2 font-bold">
+                        Password<span className="text-red-700">*</span>
+                    </label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Password"
+                        className="w-full h-10 bg-zinc-200 rounded-sm px-2 shadow-sm shadow-zinc-500 mb-5"
+                        value={data.password}
+                        onChange={handleChange}
+                    />
+
+                    {checkRequiredField && (
+                        <p className="pb-2 flex items-center justify-center gap-1 text-xs">
+                            Please check the required
+                            <span className="text-red-700">*</span> fields first
+                        </p>
+                    )}
+                    <button
+                        type="submit"
+                        className="bg-black text-white py-2 px-4 rounded-md font-bold flex items-center justify-center m-auto 
+            shadow-md shadow-zinc-500 hover:text-red-700 active:shadow-none"
+                    >
+                        Login
+                    </button>
+                </form>
+                <p className="flex items-center justify-center pb-5 text-sm">
+                    Don't have Account?{" "}
+                    <Link
+                        to={"/signup"}
+                        className="ml-2 text-red-700 hover:underline hover:text-red-700 active:no-underline font-bold"
+                    >
+                        SignUp
+                    </Link>
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default Login;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import loginGifAnimation from "../assets/images/logo/loginLogo.gif";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Signup() {
+    const navigate = useNavigate();
+    const [data, setData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+    const [checkRequiredField, setCheckReqField] = useState(false);
+    const [passwordMatch, setPassswordMatch] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { firstName, email, password, confirmPassword } = data;
+        if (firstName && email && password && confirmPassword) {
+            setCheckReqField(false);
+            if (password === confirmPassword) {
+                console.log(data);
+                alert("successfull");
+                navigate("/login");
+            } else {
+                setPassswordMatch(true);
+            }
+        } else {
+            setCheckReqField(true);
+            setPassswordMatch(false);
+            // alert("Check Required * fields first");
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setData((prev) => {
+            return {
+                ...prev,
+                [name]: value,
+            };
+        });
+    };
+
     return (
         <div className="flex h-full justify-center items-center">
             <div className=" pt-3 w-full max-w-sm bg-white rounded-sm shadow-sm shadow-zinc-500">
@@ -13,9 +53,12 @@ function Signup() {
                         className=" animate-wiggle"
                     />
                 </div>
-                <form action="" className=" mx-8 pb-4">
+                <form action="" className=" mx-8 pb-4" onSubmit={handleSubmit}>
                     <label htmlFor="firstName" className="pl-2 font-bold">
-                        First Name
+                        First Name<span className="text-red-700">*</span>
+                        <span className=" text-sm font-normal">
+                            (Indicates required field)
+                        </span>
                     </label>
                     <input
                         type="text"
@@ -23,6 +66,8 @@ function Signup() {
                         name="firstName"
                         placeholder="First Name"
                         className="w-full h-10 bg-zinc-200 rounded-sm px-2 shadow-sm shadow-zinc-500 mb-2"
+                        value={data.firstName}
+                        onChange={handleChange}
                     />
                     <label htmlFor="lastName" className="pl-2 font-bold">
                         Last Name
@@ -33,9 +78,11 @@ function Signup() {
                         name="lastName"
                         placeholder="Last Name"
                         className="w-full h-10 bg-zinc-200 rounded-sm px-2 shadow-sm shadow-zinc-500 mb-2"
+                        value={data.lastName}
+                        onChange={handleChange}
                     />
                     <label htmlFor="email" className="pl-2 font-bold">
-                        Email
+                        Email<span className="text-red-700">*</span>
                     </label>
                     <input
                         type="email"
@@ -43,9 +90,11 @@ function Signup() {
                         name="email"
                         placeholder="Email"
                         className="w-full h-10 bg-zinc-200 rounded-sm px-2 shadow-sm shadow-zinc-500 mb-2"
+                        value={data.email}
+                        onChange={handleChange}
                     />
                     <label htmlFor="email" className="pl-2 font-bold">
-                        Password
+                        Password<span className="text-red-700">*</span>
                     </label>
                     <input
                         type="password"
@@ -53,27 +102,47 @@ function Signup() {
                         name="password"
                         placeholder="Password"
                         className="w-full h-10 bg-zinc-200 rounded-sm px-2 shadow-sm shadow-zinc-500 mb-2"
+                        value={data.password}
+                        onChange={handleChange}
                     />
                     <label htmlFor="email" className="pl-2 font-bold">
-                        Confirm Password
+                        Confirm Password<span className="text-red-700">*</span>
                     </label>
+
                     <input
                         type="password"
-                        id="confirmpassword"
-                        name="confirmpassword"
+                        id="confirmPassword"
+                        name="confirmPassword"
                         placeholder="Confirm Password"
                         className="w-full h-10 bg-zinc-200 rounded-sm px-2 shadow-sm shadow-zinc-500 mb-5"
+                        value={data.confirmPassword}
+                        onChange={handleChange}
                     />
+                    {checkRequiredField && (
+                        <p className="pb-2 flex items-center justify-center gap-1 text-xs">
+                            Please check the required
+                            <span className="text-red-700">*</span> fields first
+                        </p>
+                    )}
+                    {passwordMatch && (
+                        <p className="pb-2 flex items-center justify-center gap-1 text-xs text-red-700">
+                            Password and confirm password does not match
+                        </p>
+                    )}
                     <button
+                        type="submit"
                         className="bg-black text-white py-2 px-4 rounded-md font-bold flex items-center justify-center m-auto 
                     shadow-md shadow-zinc-500 hover:text-red-700 active:shadow-none"
                     >
-                        Submit
+                        Sign Up
                     </button>
                 </form>
                 <p className="flex items-center justify-center pb-5 text-sm">
                     Already have Account?{" "}
-                    <Link className="ml-2 text-red-700 hover:underline hover:text-red-700 active:no-underline font-bold">
+                    <Link
+                        to={"/login"}
+                        className="ml-2 text-red-700 hover:underline hover:text-red-700 active:no-underline font-bold"
+                    >
                         Login
                     </Link>
                 </p>
